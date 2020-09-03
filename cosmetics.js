86cd09d1795b2d6098d1a76ad2abc98ec95d1c60
saipavz <47077235+saipavz@users.noreply.github.com>
2020-09-03 13:22:39 +0530

    Add files via upload
    A homepage for a cosmetics brand has been built.It is made such that the important products are showcased very well.

---
+++ cosmetics.js
@@ -0,0 +1,16 @@
+let butt = document.querySelector("#special");
+
+butt.addEventListener("click",function() 
+{
+	alert("Login in into the website!");
+});
+let but = document.querySelector("#button1");
+
+but.addEventListener("click",function() 
+{
+	alert("Login into the website");
+});
+ let anchor = document.querySelector("a");
+ a.addEventListener("click",function() {
+ 	alert("product list must be updated!");
+ });
